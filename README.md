ccd7c54172e8c9ba58e519d510a686bb0511ce0d
David Merrick <david@david-merrick.com>
2022-10-20 17:43:51 -0700

    Just use the datetime_digitized

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Some Python utils for handling FastFoto scans.
 ```shell
 pip3 install -r requirements.txt
 ```
+
+# Usage
+
+Use these just like a standard UNIX shell script
+
+```shell
+echo ~/Downloads/1995_Summer_The\ Merricks_0001_a.jpg | ./bin/exifDate.py
+```
--- bin/exifDate.py
+++ bin/exifDate.py
@@ -1,14 +1,12 @@
 #!/usr/local/bin/python3
 
 # Takes the filename from stdin
-# Burns the EXIF date based on the filename
-# Example: 
-# 2020_Summer.jpeg
+# FastFoto sets the "datetime_digitized" prop of the file already,
+# so we can use that to set the original date
 
 import sys
 from pathlib import Path    
 from exif import Image
-import re
 
 def main():
     for line in sys.stdin:
@@ -19,24 +17,19 @@ def main():
             sys.exit(1)
 
         fileName = imageFile.name
-        year = parseYear(fileName)
         burnExifDate(filePath)
-
-def parseDate(fileName):
-    year = parseYear(fileName)
-    month = parseMonth(fileName)
-
-def parseYear(fileName):
-    return re.search(r"^\d\d\d\d", fileName).group()
-
-def parseMonth(fileName):
-    return re.search("^\d\d\d\d", fileName).group()
 
 # Bail if image already has date
 # Todo: Add a "--force" flag to this script
 def burnExifDate(filePath):
     img = Image(filePath)
-    print(sorted(img.list_all())
+    if img.get("datetime_original"):
+        print("Image already contains date. Bailing.")
+        sys.exit(1)
+    digitizedDate = img.get("datetime_digitized")
+    img.datetime_original = digitizedDate
+    with open(filePath, 'wb') as new_image_file:
+        new_image_file.write(img.get_file())
 
 if __name__ == '__main__':
     main()
